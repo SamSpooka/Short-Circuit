9b92167bfc6ed3570b499223bbbef3075ea47080
Sam Spika <samspooka@gmail.com>
2022-10-03 19:42:09 -0500

    Feature-WireUIBacktracking - Added ability to backtrack, appears to be working completely

--- Assets/Scripts/Wire.cs
+++ Assets/Scripts/Wire.cs
@@ -13,12 +13,15 @@ public class Wire : MonoBehaviour
     private Vector2 prevEndPoint;
     [SerializeField] private float WIRE_SEGMENT_LENGTH_THRESHOLD = 3f;
     [SerializeField] private float WIRE_SEGMENT_BREAK_THRESHOLD = 1f;
+    [SerializeField] private float WIRE_SEGMENT_BACKTRACK_THRESHOLD = 0.5f;
     private float GRID_SIZE = 0.1f;
     // Start is called before the first frame update
     void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
         currentDirection = Vector2.down;
+        if (WIRE_SEGMENT_BACKTRACK_THRESHOLD >= WIRE_SEGMENT_BREAK_THRESHOLD)
+            Debug.Log("WARNING: BACKTRACK_THRESHOLD should not be less than or equal to BREAK_THRESHOLD for best experience");
     }
 
     // Update is called once per frame
@@ -85,6 +88,7 @@ public class Wire : MonoBehaviour
 
         float absVertShift = Mathf.Abs(lastSetPoint.y - prevPoint.y);
         float absHorizShift = Mathf.Abs(lastSetPoint.x - prevPoint.x);
+        //Debug.Log("Direction: " + currentDirection);
 
         if (currentDirection == Vector2.up || currentDirection == Vector2.down) //Currently moving vertically
         {
@@ -92,8 +96,21 @@ public class Wire : MonoBehaviour
             {
                 CreateNewLinePosition(new Vector2(lastSetPoint.x, endPoint.y)); //Create a new point only shifted in the y direction
                 currentDirection = Vector2.right * Mathf.Sign(endPoint.x - lastSetPoint.x); //Now moving either left or right
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, new Vector2(endPoint.x, endPoint.y));
             }
-            lineRenderer.SetPosition(lineRenderer.positionCount - 1, new Vector2(lastSetPoint.x, endPoint.y));
+            else if (absVertShift <= WIRE_SEGMENT_BACKTRACK_THRESHOLD && lineRenderer.positionCount > 2) //Should backtrack (change to horizontal movement)
+            {
+                Debug.Log("Removing Vertical, switching to horizontal");
+                RemoveLastLinePosition();
+                Vector2 previousLastSetPoint = lineRenderer.GetPosition(lineRenderer.positionCount - 2);
+                if (previousLastSetPoint.x < endPoint.x)
+                    currentDirection = Vector2.right;
+                else
+                    currentDirection = Vector2.left;
+            }
+            else
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, new Vector2(lastSetPoint.x, endPoint.y));
+            
         }
         else if (currentDirection == Vector2.left || currentDirection == Vector2.right) //Currently moving horizontally
         {
@@ -101,8 +118,21 @@ public class Wire : MonoBehaviour
             {
                 CreateNewLinePosition(new Vector2(endPoint.x, lastSetPoint.y)); //Create a new point only shifted in the x direction
                 currentDirection = Vector2.up * Mathf.Sign(endPoint.y - lastSetPoint.y); //Now moving either up or down
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, new Vector2(endPoint.x, endPoint.y));
             }
-            lineRenderer.SetPosition(lineRenderer.positionCount - 1, new Vector2(endPoint.x, lastSetPoint.y));
+            else if (absHorizShift <= WIRE_SEGMENT_BACKTRACK_THRESHOLD && lineRenderer.positionCount > 2) //Should backtrack (change to vertical movement)
+            {
+                Debug.Log("Removing Horizontal, switching to vertical");
+                Debug.Log("Prev,last" + prevPoint + ", " + lastSetPoint);
+                RemoveLastLinePosition();
+                Vector2 previousLastSetPoint = lineRenderer.GetPosition(lineRenderer.positionCount - 2);
+                if (previousLastSetPoint.y < endPoint.y)
+                    currentDirection = Vector2.up;
+                else
+                    currentDirection = Vector2.down;
+            }
+            else
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, new Vector2(endPoint.x, lastSetPoint.y));
         } 
 
         prevEndPoint = endPoint;
@@ -117,6 +147,16 @@ public class Wire : MonoBehaviour
         newPositions[lineRenderer.positionCount - 1] = newPos; //Add the new position at the end of the array (with 1 buffer space for the point to be moved)
         lineRenderer.positionCount++;
         lineRenderer.SetPositions(newPositions); //Update the line renderer
+    }
+
+    private void RemoveLastLinePosition()
+    {
+        Vector3[] newPositions = new Vector3[lineRenderer.positionCount - 1]; //Create new array with 1 fewer position than the lineRenderer had previously
+        for (int i = 0; i < lineRenderer.positionCount - 1; i++)
+            newPositions[i] = lineRenderer.GetPosition(i);
+
+        lineRenderer.positionCount--;
+        lineRenderer.SetPositions(newPositions);
     }
 
     private void OnDrawGizmos()
